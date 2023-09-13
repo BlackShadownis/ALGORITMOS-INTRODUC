30dec86fed3d6f5d00c8e8859bf4acb1054ad7ed
BlackShadownis <142626530+BlackShadownis@users.noreply.github.com>
2023-09-13 11:53:51 -0500

    Create ejercicio5.cpp

---
+++ ejercicio5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int contador = 0;
+
+  while (contador < 100) {
+    contador = contador + 1;
+    cout << contador << "\n";
+  }
+  
+}
