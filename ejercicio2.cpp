be92f5b1298fa7ece3b08ac8ca86cdc67c3ae164
BlackShadownis <142626530+BlackShadownis@users.noreply.github.com>
2023-09-13 11:51:49 -0500

    ejercicio2.cpp

---
+++ ejercicio2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string nombre;
+  cout << "escribe tu nombre: ";
+  cin >> nombre;
+ 
+  cout << "HOLA, " + nombre; 
+  return 0;
+
+
+}
