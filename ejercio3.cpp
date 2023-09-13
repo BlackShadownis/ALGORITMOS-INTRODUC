e5e626d37e5015d4a0169f9d34268907b8508ff0
BlackShadownis <142626530+BlackShadownis@users.noreply.github.com>
2023-09-13 11:52:29 -0500

    ejercio3.cpp

---
+++ ejercio3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+  
+  int x;
+  int y; 
+  int resultado;
+
+  cout << "escribe el valor de x: ";
+  cin >> x;
+  cout << "escribe el valor de y: ";
+  cin >> y;
+  cout << "\n";
+  resultado = x + y;
+  cout << resultado;
+
+  return 0;
+  
+}
